fc49a54b78adb7817f5e57bf30655f8ff51e43f6
abrahamx97 <abrahamx97@gmail.com>
2018-03-19 18:52:39 -0600

    create table, define database reference at start app, insert degrees values

--- src/providers/database/database.ts
+++ src/providers/database/database.ts
@@ -1,66 +1,119 @@
 import { Injectable } from '@angular/core';
 import { SQLiteObject, SQLite } from '@ionic-native/sqlite'
 
+
 @Injectable()
 export class DatabaseProvider {
 
-  database : SQLiteObject = null
+    protected database: SQLiteObject = null
 
-  constructor(public sqlite : SQLite) {
-    console.log('Hello DatabaseProvider Provider');
-  }
+    /*databaseReference variable is a static reference to database, it is initialize in platform.ready() event. Its lifecycle is the same as applications lifecycle. It avoid close or reopen database on each page/provider, each page/provider just read and copy the databaseReference on its own file.
+    */
+    private static databaseReference: SQLiteObject = null
 
-  createDatabase(){
-      return this.sqlite.create({
-          name: 'checkme.db',
-          location: 'default'
-      })
-  }
-  
-  createTables(){
-      let sql = 
-        `create table grados(
-            id_grado integer primary key,
-            grado int,
-            grado_texto varchar(20)
-        );
-        
-        create table asistencias(
-            id_asistencia integer primary key,
-            fecha varchar(20)
-        );
-        
-        create table grupos(
-            id_grupo integer primary key,
-            id_grado integer,
-            nombre varchar(40),
-            abreviacion varchar(15),
-            foreign key (id_grado) references grados(id_grado)
-        );
-        
-        create table alumnos (
-            id_alumno integer primary key,
-            id_grupo integer,
-            nombre varchar(40),
-            paterno varchar(40),
-            materno varchar(40),
-            foreign key (id_grupo) references grupos(id_grupo)
-        );
-        
-        create table detalle_asistencias(
-            id_asistencia integer,
-            id_alumno integer,
-            tipo int,
-            foreign key (id_asistencia) references asistencias(id_asistencia),
-            foreign key (id_alumno) references alumnos(id_alumno)
-        );`
+    constructor(public sqlite: SQLite) {
 
-        return this.database.executeSql(sql, {})
-  }
-  setDatabase(database : SQLiteObject){
-      if(this.database === null){
-          this.database=database
-      }
-  }
+    }
+
+    static getDatabaseReference() {
+        return this.databaseReference
+    }
+
+    static setDatabaseReference(database: SQLiteObject) {
+        this.databaseReference = database
+    }
+
+    createDatabase() {
+        return this.sqlite.create({
+            name: 'checkme.db',
+            location: 'default'
+        })
+    }
+
+    getDatabase() {
+        return this.database
+    }
+
+    createTables() {
+        let degrees =`create table if not exists degrees(
+                degree_id integer primary key,
+                degree int,
+                degree_text varchar(20)
+            );`
+        let attendances=`
+            create table if not exists attendances(
+                attenndance_id integer primary key,
+                attendance_date date
+            );`
+            
+        let groups=`create table if not exists groups(
+                group_id integer primary key autoincrement,
+                degree_id integer,
+                name varchar(40),
+                foreign key (degree_id) references degrees(degree_id)
+            );`
+            
+        let students = `create table if not exists students (
+                student_id integer primary key,
+                group_id integer,
+                ID varchar(30),
+                gender varchar(2),
+                name varchar(100),
+                foreign key (group_id) references groups(group_id)
+            );`
+            
+        let attendance_detail=`create table if not exists attendace_detail(
+                attendance_id integer,
+                student_id integer,
+                type varchar(2),
+                foreign key (attendance_id) references attendances(attendance_id),
+                foreign key (student_id) references students(student_id)
+            );`
+        return this.database.executeSql(degrees, {}).then(result=>{
+            return this.database.executeSql(attendances, {})
+        }).then(result=>{
+            return this.database.executeSql(groups, {})
+        }).then(result=>{
+            return this.database.executeSql(students, {})
+        }).then(result=>{
+            return this.database.executeSql(attendance_detail, {})
+        })
+    }
+
+    setDatabase(database: SQLiteObject) {
+        DatabaseProvider.setDatabaseReference(database)
+        this.database = DatabaseProvider.getDatabaseReference()
+    }
+
+
+    //insert default degrees
+    insertDegrees() : Promise<any>{
+        return new Promise<any>((resolve, reject)=>{
+            let sql = `insert into degrees(degree_id, degree, degree_text) values (1,1,'Primero'), (2,2,'Segundo'), (3,3,'Tercero'), (4,4,'Cuarto'), (5,5,'Quinto'), (6,6,'Sexto'), (7,7,'Septimo'), (8,8,'Octavo'), (9,9,'Noveno'), (10,10,'Décimo')`
+            this.database.executeSql('select count(*) as rows from degrees', {}).then(data=>{
+                if(data.rows.item(0).rows>=10){
+                    resolve(true);
+                }else{
+                    return this.database.executeSql(sql, {})
+                }
+            }).then(result=>{
+                if(result.rowsAffected>0){
+                    resolve(true)
+                }else{
+                    resolve(false)
+                }
+            }).catch(error=>{
+                reject(error)
+            })
+        })       
+    }
+
+    closeDatabase() {
+        return this.database.close()
+    }
+
+
+
+
 
 }
