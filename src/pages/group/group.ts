97cc8fa1129192d92e11033ff4251ff99d0f962e
abrahamx97 <abrahamx97@gmail.com>
2018-03-19 19:07:04 -0600

    list students for a specific group

--- src/pages/group/group.ts
+++ src/pages/group/group.ts
@@ -1,34 +1,49 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { GroupsProvider } from '../../providers/groups/groups'
+
 @IonicPage()
 @Component({
-  selector: 'page-group',
-  templateUrl: 'group.html',
+    selector: 'page-group',
+    templateUrl: 'group.html',
 })
+
 export class GroupPage {
 
-  students: Array<{ id: number, name: string, absences: number }>
-  options: Array<{ icon: string, color: string, defaultColor: string }>
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.students = [
-      { id: 1, name: 'Abraham Gutiérrez Lara', absences: 13 },
-      { id: 2, name: 'José Manuel Chávez Castillo', absences: 1 },
-      { id: 3, name: 'Martin Vicente Santos', absences: 4 }
-    ]
+    /*attendanceTypes for internal use
+      'P': came to classroom,
+      'A': absent,
+      'J': justified,
+      'I': ignore day
+    */
 
-    
+    students = new Array<{ id: number, name: string, absences?: number, attendanceType?: 'P' }>()
+    options = new Array<{ icon: string, color: string, defaultColor: string }>()
+    fullname = 'Grupo'
 
-    this.options = [
-      { icon: 'checkmark-circle', color: 'secondary', defaultColor: 'secondary'},
-      {icon: 'clock', color: 'warning', defaultColor: 'dark'},
-      {icon: 'create', color: 'primary', defaultColor: 'dark'}, 
-      {icon: 'close-circle', color: 'danger', defaultColor: 'dark'}
-    ]
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams, public groupsProvider: GroupsProvider) {
+        this.fullname=this.navParams.data.fullname
+        this.groupsProvider.find(this.navParams.data.group_id).then(students => {
+            let dataStudents = new Array<any>()
+            for (let i = 0; i < students.rows.length; i++) {
+                dataStudents.push(students.rows.item(i))
+            }
+            this.students=dataStudents
+        }).catch(error => {
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad GroupPage');
-  }
+        })
+
+        this.options = [
+            { icon: 'checkmark-circle', color: 'secondary', defaultColor: 'secondary' },
+            { icon: 'clock', color: 'warning', defaultColor: 'dark' },
+            { icon: 'create', color: 'primary', defaultColor: 'dark' },
+            { icon: 'close-circle', color: 'danger', defaultColor: 'dark' }
+        ]
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad GroupPage');
+    }
 
 }
