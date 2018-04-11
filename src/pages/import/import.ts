12e20044b8d38ae9b9070def8b27e5162e6dcb93
abrahamx97 <abrahamx97@gmail.com>
2018-04-11 01:50:48 -0500

    design changes

--- src/pages/import/import.ts
+++ src/pages/import/import.ts
@@ -68,6 +68,8 @@ export class ImportPage {
         }).catch(error => {
             this.saveStundentsErrorHandler()
         })
+
+        form.reset()
     }
 
     private saveStundentsErrorHandler() {
